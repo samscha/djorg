9753d42aabcfe9074a80fb64902f498978cafd83
Samuel Cha <sunchipssam@gmail.com>
2018-05-08 14:49:49 -0700

    Fixed print history bugs

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -7,33 +7,37 @@ class Bookmark(models.Model):
     url = models.URLField('URL', unique=True)
     name = models.CharField(max_length=200)
     notes = models.TextField(blank=True)
-    creation_date = models.DateTimeField('date created', editable=False)
+    created = models.DateTimeField('date created', editable=False)
     history = models.TextField(blank=True)
-
-    # def __init__(self, name, url, creation_date):
-    # self.name = name
-    # self.url = url
-    # self.creation_date = creation_date
-    # self.history = ' ' + 'name_created:' + self.name
-    # self.history = self.history + ' ' + 'url_created:' + self.url
 
     def __str__(self):
         return "(" + self.name + ": " + self.url + ")"
 
     def edit_name(self, new_name):
-        self.history = self.history + ' ' + 'name_changed:' + self.name
+        if (self.history == ''):
+            self.history += 'name_created: ' + self.name
+            self.history += '{}url_created:' + self.url
+
+        self.history += '{}' + 'name_changed:' + self.name
         self.name = new_name
 
     def edit_url(self, new_url):
-        self.history = self.history + ' ' + 'url_changed:' + self.url
+        if (self.history == ''):
+            self.history += 'name_created: ' + self.name
+            self.history += '{}url_created: ' + self.url
+
+        self.history += '{}' + 'url_changed: ' + self.url
         self.url = new_url
 
     def print_history(self):
         if (self.history == ''):
-            print('No history written.')
-            print('name_created: %s url_created: %s' % (self.name, self.url))
+            self.history += 'name_created: ' + self.name
+            self.history += '{}url_created: ' + self.url
 
-        hist = self.history.split(' ')
+        hist = self.history.split('{}')
         for i, item in enumerate(hist):
             print("%d: %s" % (i, item))
-        print('current_name: %s current_url: %s' % (self.name, self.url))
+
+        print('------end')
+        print('current_name: %s' % self.name)
+        print('current_url: %s' % self.url)
