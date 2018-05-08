200201847883aa139ec6f08fec194101d71d4355
Samuel Cha <sunchipssam@gmail.com>
2018-05-08 11:07:45 -0700

    Added bookmark model

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,9 @@
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+
+class Bookmark(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    url = models.URLField('URL', unique=True)
+    name = models.CharField(max_length=200)
+    notes = models.TextField(blank=True)
