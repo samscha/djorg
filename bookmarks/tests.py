d354fd5b1044e5b38a219168fd5cd22302d166ce
Samuel Cha <sunchipssam@gmail.com>
2018-05-16 10:25:46 -0700

    Added bookmark testing

--- bookmarks/tests.py
+++ bookmarks/tests.py
@@ -1,3 +1,39 @@
+from django.db.utils import IntegrityError
 from django.test import TestCase
+from .models import Bookmark
 
-# Create your tests here.
+
+class BookmarkTestCase(TestCase):
+    def setUp(self):
+        Bookmark.objects.create(name='This is a bookmark',
+                                url='https://www.google.com')
+
+        Bookmark.objects.create(name='This is a bookmark with a note',
+                                notes='A note', url='https://www.instagram.com')
+
+    def test_retrieving_all_bookmarks(self):
+        bookmarks = Bookmark.objects.all()
+
+        self.assertEqual(len(bookmarks), 2)
+
+    def test_retrieving_bookmarks_by_name(self):
+        bookmark = Bookmark.objects.get(name='This is a bookmark')
+
+        self.assertEqual(bookmark.name, 'This is a bookmark')
+        self.assertEqual(bookmark.notes, '')
+        self.assertEqual(bookmark.url, 'https://www.google.com')
+
+    def test_retrieving_bookmarks_by_url(self):
+        bookmark = Bookmark.objects.get(url='https://www.instagram.com')
+
+        self.assertEqual(bookmark.name, 'This is a bookmark with a note')
+        self.assertEqual(bookmark.notes, 'A note')
+        self.assertEqual(bookmark.url, 'https://www.instagram.com')
+
+    def test_dupe_urls(self):
+        with self.assertRaises(IntegrityError) as context:
+            Bookmark.objects.create(name='This is an error bookmark',
+                                    url='https://www.google.com')
+
+        self.assertTrue(
+            'UNIQUE constraint failed: bookmarks_bookmark.url' in str(context.exception))
